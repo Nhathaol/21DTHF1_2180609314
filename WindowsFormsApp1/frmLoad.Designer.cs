31bbf9f1fa0fcdfa379ef29bc2671c466f14e90c
nghiatndh <hieunghia248@gmail.com>
2023-10-16 10:17:32 +0700

    added load form sinhvien

---
+++ WindowsFormsApp1/frmLoad.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class frmLoad
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.MaSV = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.HoTen = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Khoa = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DiemTB = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.MaSV,
+            this.HoTen,
+            this.Khoa,
+            this.DiemTB});
+            this.dataGridView1.Location = new System.Drawing.Point(-2, 30);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(955, 475);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // MaSV
+            // 
+            this.MaSV.HeaderText = "MaSV";
+            this.MaSV.MinimumWidth = 6;
+            this.MaSV.Name = "MaSV";
+            this.MaSV.Width = 125;
+            // 
+            // HoTen
+            // 
+            this.HoTen.HeaderText = "HoTen";
+            this.HoTen.MinimumWidth = 6;
+            this.HoTen.Name = "HoTen";
+            this.HoTen.Width = 125;
+            // 
+            // Khoa
+            // 
+            this.Khoa.HeaderText = "Khoa";
+            this.Khoa.MinimumWidth = 6;
+            this.Khoa.Name = "Khoa";
+            this.Khoa.Width = 125;
+            // 
+            // DiemTB
+            // 
+            this.DiemTB.HeaderText = "DiemTB";
+            this.DiemTB.MinimumWidth = 6;
+            this.DiemTB.Name = "DiemTB";
+            this.DiemTB.Width = 125;
+            // 
+            // frmLoad
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(956, 529);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "frmLoad";
+            this.Text = "frmLoad";
+            this.Load += new System.EventHandler(this.frmLoad_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MaSV;
+        private System.Windows.Forms.DataGridViewTextBoxColumn HoTen;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Khoa;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DiemTB;
+    }
+}
---
+++ WindowsFormsApp1/frmLoad.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class frmLoad : Form
+    {
+        public frmLoad()
+        {
+            InitializeComponent();
+        }
+
+        private void frmLoad_Load(object sender, EventArgs e)
+        {
+            Model3 context = new Model3();
+            List<Student> liststudents = context.Students.ToList();
+            dataGridView1.Rows.Clear();
+            foreach(var item in liststudents)
+            {
+                int index = dataGridView1.Rows.Add();
+
+                dataGridView1.Rows[index].Cells["MaSV"].Value = item.StudentID;
+                dataGridView1.Rows[index].Cells["HoTen"].Value = item.FullName;
+                dataGridView1.Rows[index].Cells["Khoa"].Value = item.FacultyID;
+                dataGridView1.Rows[index].Cells["DiemTB"].Value = item.AverageScore;
+            }
+        }
+    }
+}
